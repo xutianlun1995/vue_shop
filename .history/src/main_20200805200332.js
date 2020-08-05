d70a8057034222cf9d63ed82da8fc7234c3be9c4
xutianlun1995 <519253779@qq.com>
2020-08-05 21:44:28 +0800

    init shop

---
+++ .history/src/main_20200805200332.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import {Button} from 'mint-ui'
+import VueLazyload from 'vue-lazyload'
+import App from './App'
+import router from './router'
+import store from './store'
+
+import './mock/mockServer' // 加载mockServer即可
+
+// 注册全局组件标签
+Vue.component(Button.name, Button)  // <mt-button>
+
+ue.use(VueLazyload, { // 内部自定义一个指令lazy
+    loading
+  })
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router, //使用上vue-router
+    store, // 使用上vuex
+})
